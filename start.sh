3fd7d16720aa5e59ee4d8271548ee10649c449c0
xiongyu <xiongyu@espressif.com>
2022-06-22 16:32:26 +0800

    fix projects path

--- start.sh
+++ start.sh
@@ -8,4 +8,10 @@ if [[ "$(sudo docker images -q "${IMAGE_NAME}:${IMAGE_VERSION}" 2> /dev/null)" =
   ${WORK_PATH}/tools/gdocker/gdocker.sh install -n ${IMAGE_NAME} -v ${IMAGE_VERSION} -t ${WORK_PATH}/tools/tools.sh
 fi
 
-${WORK_PATH}/tools/gdocker/gdocker.sh start -n ${IMAGE_NAME} -v ${IMAGE_VERSION} -c "/bin/bash /home/${IMAGE_NAME}/workspace/sessions.sh" -p ${WORK_PATH}/projects
+if [[ "$1" == "" ]]; then
+  PROJECT=${WORK_PATH}/projects
+else
+  PROJECT=$1
+fi
+
+${WORK_PATH}/tools/gdocker/gdocker.sh start -n ${IMAGE_NAME} -v ${IMAGE_VERSION} -c "/bin/bash /home/${IMAGE_NAME}/workspace/sessions.sh" -p $PROJECT
