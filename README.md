15fa5a7174b5ba775c11c02838321e4df3af9f57
xiongyu <xiongyu@espressif.com>
2022-06-22 16:13:47 +0800

    esp-idf v4.3.3

---
+++ README.md
@@ -0,0 +1,63 @@
+# _ESP32-HMI_
+
+[![CI](https://github.com/xiongyumail/esp-hmi/actions/workflows/main.yml/badge.svg)](https://github.com/xiongyumail/esp-hmi/actions/workflows/main.yml)
+
+## Contents
+
+    * projects
+
+      Development examples
+
+    * tools
+
+      * esp-idf sdk
+      * gdocker
+      * lvgl tool
+
+    * Data
+
+      * Schematic diagram
+
+      * Misc
+
+## How to use
+
+You can follow the steps below to set up the development environment, or directly download the release version of the full environment.
+
+* docker.io
+
+  ```
+  sudo apt install docker.io
+  ```
+
+* clone
+
+  ```bash
+  git clone --recursive https://github.com/xiongyumail/esp32-hmi
+  cd esp32-hmi
+  ```
+
+  * note
+
+    Don't omit `--recursive`, because we use submodule.
+
+* update
+
+  ```bash
+  git pull
+  git submodule update --init --recursive
+  ```
+
+* Run
+
+  ```bash
+  ./start.sh
+  ```
+
+## Appendix
+
+### Schematic
+
+### Datasheet
+
+* [CAM-OV2640](docs/OV2640_DS+(2.2).pdf)
---
+++ tools/tools.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+WORK_PATH=$(cd $(dirname $0); pwd)
+TEMP_PATH=~/workspace/.tmp/${MY_NAME}
+echo "WORK_PATH: ${WORK_PATH}"
+echo "TEMP_PATH: ${TEMP_PATH}"
+
+sudo apt-get update
+sudo chown -R ${MY_NAME}:${MY_NAME} /home/${MY_NAME}/workspace
+
+if [ ! -d "${TEMP_PATH}" ]; then
+   mkdir -p ${TEMP_PATH}
+fi
+cd ${TEMP_PATH}
+if [ ! -d ".config" ]; then
+   mkdir .config
+fi
+if [ ! -d ".tools" ]; then
+   mkdir .tools
+fi
+
+cd ${TEMP_PATH}/.config
+if [ ! -d ".config" ]; then
+   mkdir .config
+   sudo rm -rf ~/.config
+   sudo ln -s $PWD/.config ~/.config
+fi
+if [ ! -d ".tmux" ]; then
+   mkdir .tmux
+   sudo rm -rf ~/.tmux
+   sudo ln -s $PWD/.tmux ~/.tmux
+fi
+if [ ! -d ".espressif" ]; then
+   mkdir .espressif
+   sudo rm -rf ~/.espressif
+   sudo ln -s $PWD/.espressif ~/.espressif
+fi
+
+# tmux
+sudo DEBIAN_FRONTEND=noninteractive apt-get install -y \
+   tmux \
+   vim-gtk 
+if [ ! -f "${TEMP_PATH}/.tools/tmux" ]; then
+   cd ${WORK_PATH}
+   cd tmux
+   ln -s $PWD/.tmux.conf ~/.tmux.conf
+   ln -s $PWD/.vimrc ~/.vimrc
+   echo "export TMUX_PATH=${TEMP_PATH}/tmux" >> ${HOME}/.bashrc
+   echo "tmux install ok" >> ${TEMP_PATH}/.tools/tmux
+fi
+
+# esp-idf
+sudo apt-get install -y \
+   python3 \
+   python3-pip \
+   cmake \
+   libusb-1.0
+if [ ! -f "${TEMP_PATH}/.tools/esp-idf" ]; then
+   cd ${WORK_PATH}
+   cd esp-idf
+   sudo rm -rf /usr/bin/python
+   sudo ln -s /usr/bin/python3 /usr/bin/python
+   ./install.sh
+   echo "export IDF_PATH=${WORK_PATH}/esp-idf" >> ${HOME}/.bashrc
+   echo "esp-idf install ok" >> ${TEMP_PATH}/.tools/esp-idf
+fi
+
+sudo apt-get clean
+sudo apt-get autoclean
+sudo rm -rf /tmp/*
+sudo rm -rf /var/tmp/*
+sudo rm -rf /var/cache/*
+sudo rm -rf /var/lib/apt/lists/*
